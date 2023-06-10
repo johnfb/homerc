6afdcaa0e4d6351f5e460f60fc25fbbc8f3e29df
John Bridgman <johnfbiii@gmail.com>
2023-06-10 10:46:50 -0400

    Update gpg init

--- init/gpg.sh
+++ init/gpg.sh
@@ -1,5 +1,7 @@
 
-GPG_TTY=$(tty)
+if tty -s; then
+    GPG_TTY=$(tty)
+fi
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
     SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
